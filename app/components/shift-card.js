928aeb1a05170d8a9c96c31bb61ae1f8f5c0338c
alpharahl <david.m.gauntlett@gmail.com>
2017-10-14 12:00:27 -0400

    Cards working with fake data

--- app/components/shift-card.js
+++ app/components/shift-card.js
@@ -3,6 +3,17 @@ import Component from '@ember/component';
 export default Component.extend({
   classNameBindings:['onShift'],
   classNames:['shift'],
+
+  splitName: Ember.computed('shift.name', function(){
+    return this.get('shift.name').split('#');
+  }),
+  shiftName: Ember.computed('shift.splitName', function(){
+    return this.get('splitName')[0];
+  }),
+  subDepartment: Ember.computed('shift.splitName',function(){
+    return this.get('splitName')[1];
+  }),
+
   actions: {
     checkIn(){
       this.set('onShift', 'worked')
@@ -11,6 +22,10 @@ export default Component.extend({
     noShow(){
       this.set('onShift', 'vacant')
       console.log("no show")
+    },
+    noData(){
+      this.set('onShift', '')
+      console.log("no data")
     }
   }
 });
--- app/routes/shifts.js
+++ app/routes/shifts.js
@@ -2,6 +2,31 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
 	model() {
-		return ['shift 1', 'shift 2', 'shift 3']
+		return [{
+			"name":"Panels 1 AV Tech#AV",
+			"timeStart":"Fri, 05 Jan 2018 12:00:00",
+			"timeEnd":"Fri, 05 Jan 2018 16:00:00",
+			"staff":[{
+				"name":"Dave G"
+			},{
+			  "name":"Afton S"
+			}]
+		},{
+			"name":"Panels 2 AV Tech#AV",
+			"timeStart":"Fri, 05 Jan 2018 10:00:00",
+			"timeEnd":"Fri, 05 Jan 2018 14:00:00",
+			"staff":[{
+				"name":"Dalton W"
+			},{
+				"name":"Lauren W"
+			}]
+		},{
+			"name":"Network Support#Network",
+			"timeStart":"Fri, 05 Jan 2018 12:00:00",
+			"timeEnd":"Fri, 05 Jan 2018 16:00:00",
+			"staff":[{
+				"name":"Cody L"
+			}]
+		}]
 	}
 });
