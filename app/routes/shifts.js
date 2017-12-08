df6c66947d1e04a8f98d1ba3377b3b7b18786552
alpharahl <david.m.gauntlett@gmail.com>
2017-12-07 19:37:59 -0500

    Pushing updates from con

--- app/routes/shifts.js
+++ app/routes/shifts.js
@@ -4,17 +4,41 @@ export default Route.extend({
 	model() {
 		return [{
 			"name":"Panels 1 AV Tech#AV",
-			"timeStart":"Fri, 05 Jan 2018 12:00:00",
-			"timeEnd":"Fri, 05 Jan 2018 16:00:00",
+			"timeStart":"12:00 PM",
+			"timeEnd":"4:00 PM",
 			"staff":[{
 				"name":"Dave G"
+			},{
+			  "name":"Afton S"
+			},{
+				"name":"Another"
+			},{
+				"name":"And Another"
+			},{
+				"name":"Kristi"
+			},{
+			  "name":"Afton S"
+			},{
+				"name":"Another"
+			},{
+				"name":"And Another"
+			},{
+				"name":"Kristi"
+			},{
+			  "name":"Afton S"
+			},{
+				"name":"Another"
+			},{
+				"name":"And Another"
+			},{
+				"name":"Kristi"
 			},{
 			  "name":"Afton S"
 			}]
 		},{
 			"name":"Panels 2 AV Tech#AV",
-			"timeStart":"Fri, 05 Jan 2018 10:00:00",
-			"timeEnd":"Fri, 05 Jan 2018 14:00:00",
+			"timeStart":"10:00 AM",
+			"timeEnd":"2:00 PM",
 			"staff":[{
 				"name":"Dalton W"
 			},{
@@ -22,10 +46,24 @@ export default Route.extend({
 			}]
 		},{
 			"name":"Network Support#Network",
-			"timeStart":"Fri, 05 Jan 2018 12:00:00",
-			"timeEnd":"Fri, 05 Jan 2018 16:00:00",
+			"timeStart":"12:00 PM",
+			"timeEnd":"4:00 PM",
 			"staff":[{
 				"name":"Cody L"
+			}]
+		},{
+			"name":"Manager Shift 15",
+			"timeStart":"12:00 PM",
+			"timeEnd":"4:00 PM",
+			"staff":[{
+				"name":"JD"
+			}]
+		},{
+			"name":"Manager Shift 16",
+			"timeStart":"2:00 PM",
+			"timeEnd":"6:00 PM",
+			"staff":[{
+				"name":"That Guy"
 			}]
 		}]
 	}
